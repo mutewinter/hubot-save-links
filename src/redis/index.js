00eb9dc1da6b61f7db56b0ae2ea5e3dcede7489b
Jeremy Mack <hi@mutewinter.com>
2016-12-15 11:38:42 -0600

    feat: support redis cloud

--- src/redis/index.js
+++ src/redis/index.js
@@ -2,7 +2,7 @@ var redis = require('redis');
 var url = require('url');
 var debug = require('debug')('save-links');
 
-var redisUrl = process.env.REDISTOGO_URL || 'redis://localhost:6379';
+var redisUrl = process.env.REDISTOGO_URL || process.env.REDISCLOUD_URL || 'redis://localhost:6379';
 var redisInfo = url.parse(redisUrl, true);
 var client = redis.createClient(redisInfo.port, redisInfo.hostname, {no_ready_check: true});
 
